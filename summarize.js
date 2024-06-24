a98e1191810835931992a83b00d80d7f4886163a
Mad-iq <149935798+Mad-iq@users.noreply.github.com>
2024-06-24 22:01:30 +0530

    Update summarize.js

--- summarize.js
+++ summarize.js
@@ -1,10 +1,7 @@
-// This is the function where the call to the API is made. Returns the summarized text as a string.
 const axios = require('axios');
 require('dotenv').config();
 
 async function summarizeText(text) {
-
-  // INSERT CODE SNIPPET FROM POSTMAN BELOW
   
   let data = JSON.stringify({
     "inputs": text,
@@ -32,11 +29,8 @@ async function summarizeText(text) {
     catch (error) {
       console.log(error);
     }
-  
-
 
 }
 
-// Allows for summarizeText() to be called outside of this file
 
 module.exports = summarizeText;
